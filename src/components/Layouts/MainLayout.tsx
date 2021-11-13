8293bf7950e7939d01f986805bb85ff119b0f195
kris <nov.kri@yandex.ru>
2021-11-14 01:09:02 +0700

    pay

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -1,17 +1,25 @@
+import './main.css'
 import ToolBar from '../Toolbar/ToolBar'
 import SideBar from '../Sidebar/SideBar'
+import Payment from '../Payment/Payment'
+import { useState } from 'react'
 
 const MainLayout: React.FC = ({ children }) => {
+    const [open, setOpen] = useState(true)
+
     return (
         <>
             <div>
                 <ToolBar />
             </div>
 
-            <div className="main">
+            <div className={'main' + (open ? '' : ' full')}>
                 <SideBar />
-                <main>{children}</main>
-                <div></div>
+                <main>
+                    <button onClick={() => setOpen(!open)}>df</button>
+                    {children}
+                </main>
+                {open && <Payment />}
             </div>
         </>
     )
---
+++ src/components/Payment/Payment.tsx
@@ -0,0 +1,12 @@
+import './payment.css'
+
+const Payment: React.FC = () => {
+    return (
+        <div className="payment">
+            <div className="ticket"></div>
+            <div className="pay"></div>
+        </div>
+    )
+}
+
+export default Payment
