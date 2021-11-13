7873f352b1a40c768e11ee5ed07274c49a6c8ef2
kris <nov.kri@yandex.ru>
2021-11-14 00:14:16 +0700

    toolbar

--- src/components/Toolbar/ToolBar.tsx
+++ src/components/Toolbar/ToolBar.tsx
@@ -4,8 +4,15 @@ import Logo from '../../assets/img/logo.svg'
 const ToolBar: React.FC = () => {
     return (
         <div className="toolbar">
-            <img src={Logo} alt="logo" />
-            <div className="links">dfsdf sdfsd sdf</div>
+            <img className="logo" src={Logo} alt="logo" />
+            {/* todo ссылки */}
+            <div className="links">
+                <span className="link">Главная {'>'} </span>
+                <span className="link">Сеансы {'>'} </span>
+                <span className="link current">Фильм</span>
+            </div>
+            {/* todo make select */}
+            <button className="select-btn">Кинотеатр на карповке {'>'}</button>
         </div>
     )
 }
